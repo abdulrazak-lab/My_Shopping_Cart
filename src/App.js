c341fafed3036b9b28b93f1640a2ea74d2b7b09e
alhassan abdul-razak <aabdulrazak22@gmail.com>
2024-01-18 13:10:21 +0000

    clean files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import CartItem from './Components/CartItem';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HELL</h1>
+      <CartItem />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/CartItem.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const CartItem = (props) => {
+return (
+    <>
+    <div>
+    <div>
+    <img />
+    </div>
+    </div>
+    <div>
+    <div>Price: 199</div>
+    <div>Phone</div>
+    <div>Qty: 1</div>
+    </div>
+    </>
+)
+}
+
+export default CartItem
