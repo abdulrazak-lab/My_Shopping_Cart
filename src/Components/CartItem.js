4785c90670a4f972b84e813a6731c542946099b7
alhassan abdul-razak <aabdulrazak22@gmail.com>
2024-01-19 16:56:39 +0000

    Adding cart items components and styling using object

--- src/Components/CartItem.js
+++ src/Components/CartItem.js
@@ -1,11 +1,33 @@
-import React from "./react"
+import React from 'react';
 
 class CartItem extends React.Component {
   render() {
     return (
-      <div>CartItem</div>
-    )
+      <div  className="cart-item">
+        <div className="left-block">
+            <img style={styles.image} src="" alt="" />
+        </div>
+        <div className="right-block">
+            <div style={{fontSize: 25}}>Bags</div>
+            <div style={{color: "#777"}}>Price: ₵ 198</div>
+            <div style={{color: "#777"}}>Qty: 1</div>
+            <div style={{color: "#777"}}>Description: Qualitity bags</div>
+            <div className="cart-item-actions">
+                {/* Button */}
+            </div>
+        </div>
+      </div>
+    );
   }
+}
+
+const styles = {
+    image: {
+    height: 110,
+    width: 110,
+    borderRadiius: 4,
+    background: "#ccc"
+    }
 }
 
 export default CartItem
