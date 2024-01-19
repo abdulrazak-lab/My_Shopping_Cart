0af6921d9504eb2d9df5650319174ccdd46afbf8
alhassan abdul-razak <aabdulrazak22@gmail.com>
2024-01-19 20:30:35 +0000

    rendering products list in the cart items

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -2,13 +2,48 @@ import React from 'react'
 import CartItem from './CartItem'
 
 class Cart extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            products: [
+                {
+                    price: 199,
+                    title: "Mobile Phone",
+                    qty: 1,
+                    img: "",
+                    id: 1
+                },
+                {
+                    price: 1982,
+                    title: "Laptop",
+                    qty: 13,
+                    img: "",
+                    id: 2
+                },
+                {
+                    price: 109,
+                    title: "Watch",
+                    qty: 1,
+                    img: "",
+                    id: 3
+                }
+            ]
+        }
+    }
+
+
   render() {
+    const { products } = this.state;
     return (
-        <div>
-            <CartItem />
-            <CartItem />
-            <CartItem />
-            <CartItem />
+        <div className='cart'>
+            {products.map((product) => {
+                return (
+                <CartItem 
+                product={product} 
+                key={product.id} 
+                />
+                )
+            })}
         </div>
     )
   }
