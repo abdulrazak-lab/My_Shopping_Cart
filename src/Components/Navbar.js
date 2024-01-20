1566cf8360886dac53b65afed8c764711706915b
alhassan abdul-razak <aabdulrazak22@gmail.com>
2024-01-20 08:45:58 +0000

    adding nabar and object css component

---
+++ src/Components/Navbar.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+class Navbar extends React.Component {
+    render () {
+  return (
+    <div style={styles.nav}>
+        <div style={styles.cartIconContainer}>
+            <img style={styles.cartIcon} src="https://image.shutterstock.com/image-vector/shopping-cart-vector-icon-flat-260nw-1690453492.jpg" alt='cart-icon' />
+            <span style={styles.cartCount}>3</span>
+        </div>
+      
+    </div>
+  )
+}
+}
+
+const styles = {
+    cartIcon: { 
+        height: 32,
+        marginRight: 20
+    },
+    nav: {
+        height: 70,
+        background: "#4267b2",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    cartIconContainer: {
+        positon: "relative"
+    },
+    cartCount: {
+        background: "yellow",
+        borderRadius: "50",
+        padding: "4px, 8px",
+        position: "absolute",
+        right: 0,
+        top: -9
+    }
+}
+
+export default Navbar
