d0bf697ec458bceb0e55ba0e61905fdff0590d0f
alhassan abdul-razak <aabdulrazak22@gmail.com>
2024-01-20 14:26:21 +0000

    moving cart state to app and changing it to functional components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,81 @@
 import React from "react";
 import Cart from "./Components/Cart";
+import Navbar from "./Components/Navbar";
 
-function App() {
+class App extends React.Component {
+
+  constructor () {
+    super();
+    this.state = {
+        products: [
+            {
+                price: 199,
+                title: "Mobile Phone",
+                qty: 1,
+                img: "",
+                id: 1
+            },
+            {
+                price: 1982,
+                title: "Laptop",
+                qty: 13,
+                img: "",
+                id: 2
+            },
+            {
+                price: 109,
+                title: "Watch",
+                qty: 1,
+                img: "",
+                id: 3
+            }
+        ]
+    }
+}
+
+handleIncreaseQuantity = (product) => {
+    const {products} = this.state;
+    const index = products.indexOf(product);
+    products[index].qty += 1;
+    this.setState({
+    products
+    });
+}
+
+handleDecreaseQuantity = (product) => {
+    const {products} = this.state;
+    const index = products.indexOf(product)
+    if(products[index].qty === 0) {
+        return;
+    }
+    products[index].qty -= 1;
+    this.setState({
+        products
+    })
+} 
+
+handleDeleteProdcuts = (id) => {
+    const {products} = this.state;
+    const items = products.filter((item) => item.id !== id);
+    this.setState({
+        products: items
+    })
+}
+
+  render () {
+    const {products} = this.state
   return (
     <div className="App">
-      <Cart />
+      <Navbar />
+      <Cart 
+        products={products}
+       onIncreaseQuantity={this.handleIncreaseQuantity}
+       onDecreaseQuantity={this.handleDecreaseQuantity} 
+       onDeleteProdcuts={this.handleDeleteProdcuts}
+      />
     </div>
   );
+}
 }
 
 export default App;
--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,67 +1,9 @@
 import React from 'react'
 import CartItem from './CartItem'
 
-class Cart extends React.Component {
-    constructor () {
-        super();
-        this.state = {
-            products: [
-                {
-                    price: 199,
-                    title: "Mobile Phone",
-                    qty: 1,
-                    img: "",
-                    id: 1
-                },
-                {
-                    price: 1982,
-                    title: "Laptop",
-                    qty: 13,
-                    img: "",
-                    id: 2
-                },
-                {
-                    price: 109,
-                    title: "Watch",
-                    qty: 1,
-                    img: "",
-                    id: 3
-                }
-            ]
-        }
-    }
-
-    handleIncreaseQuantity = (product) => {
-        const {products} = this.state;
-        const index = products.indexOf(product);
-        products[index].qty += 1;
-        this.setState({
-        products
-        });
-    }
-
-    handleDecreaseQuantity = (product) => {
-        const {products} = this.state;
-        const index = products.indexOf(product)
-        if(products[index].qty === 0) {
-            return;
-        }
-        products[index].qty -= 1;
-        this.setState({
-            products
-        })
-    } 
-
-    handleDeleteProdcuts = (id) => {
-        const {products} = this.state;
-        const items = products.filter((item) => item.id !== id);
-        this.setState({
-            products: items
-        })
-    }
-
-  render() {
-    const { products } = this.state;
+const Cart = (props) => {
+   
+    const { products } = props;
     return (
         <div className='cart'>
             {products.map((product) => {
@@ -69,15 +11,15 @@ class Cart extends React.Component {
                 <CartItem 
                 product={product} 
                 key={product.id}
-                onIncreaseQuantity={this.handleIncreaseQuantity}
-                onDecreaseQuantity={this.handleDecreaseQuantity} 
-                onDeleteProdcuts={this.handleDeleteProdcuts}
+                onIncreaseQuantity={props.onIncreaseQuantity}
+                onDecreaseQuantity={props.onDecreaseQuantity} 
+                onDeleteProdcuts={props.onDeleteProdcuts}
                 />
                 )
             })}
         </div>
     )
-  }
+  
 }
 
 export default Cart
